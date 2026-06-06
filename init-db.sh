5eaa4c10d8a9734822eec5d838f874d0c57aef08
Pedro-Lucc <104800313+Pedro-Lucc@users.noreply.github.com>
2026-06-06 02:47:45 -0300

    up

--- init-db.sh
+++ init-db.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e
 
+export PGRST_SERVER_PORT="${PGRST_SERVER_PORT:-${PORT:-10000}}"
+
 if [ -n "${PGRST_DB_URI:-}" ] && [ -f /etc/postgrest/init-db.sql ]; then
   echo "Initializing PostgREST database..."
   until psql "$PGRST_DB_URI" -v ON_ERROR_STOP=1 -f /etc/postgrest/init-db.sql; do
